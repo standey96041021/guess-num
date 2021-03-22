579910726ab41116bffd7d077f7fa25bfffeccb5
Yan Cheng <standey12345@gmail.com>
2021-03-22 18:08:03 +0800

    second version

--- guess.py
+++ guess.py
@@ -4,7 +4,9 @@
 #猜錯的話 要告訴他 比答案大/小
 import random
 r = random.randint(1, 100)
+i = 0
 while True:
+    i = i + 1
     num = int (input('請輸入一個隨機整數1~100:'))
     if num == r:
         print('恭喜猜對了!')
@@ -13,3 +15,5 @@ while True:
         print('比答案大')
     elif num < r:
         print('比答案小')
+    print('目前已猜次數為：', i)
+
