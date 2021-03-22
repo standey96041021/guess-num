24f4b41e04874646a3572399a382de042ebeeffa
Yan Cheng <standey12345@gmail.com>
2021-03-22 18:33:15 +0800

    finish version

--- guess.py
+++ guess.py
@@ -3,11 +3,13 @@
 #猜對的話 印出 "終於猜對了!"
 #猜錯的話 要告訴他 比答案大/小
 import random
-r = random.randint(1, 100)
+start_num = int(input('請輸入開始數值:'))
+end_num = int(input('請輸入結束數值:'))
+r = random.randint(start_num, end_num)
 count = 0
 while True:
     count += 1 # count = count + 1
-    num = int (input('請輸入一個隨機整數1~100:'))
+    num = int (input('請輸入一個隨機整數'))
     if num == r:
         print('恭喜猜對了!')
         print('這是您猜的第', count, '次')
